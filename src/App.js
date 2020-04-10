f337a09c18a1f7232ebc803ae753fb8bbc856961
sushilkadu <sushil@appalladium.com>
2020-04-10 18:37:01 +0530

    Updated UI

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Yooo</p>
+        <p>This is changed for docker</p>
         <a
           className="App-link"
           href="https://reactjs.org"
